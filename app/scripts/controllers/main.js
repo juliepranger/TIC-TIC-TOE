0c44bdcca32c49b557be8203b175999e2dcfc531
Julie Pranger <juliepranger@Julies-MacBook-Pro.local>
2013-10-09 15:25:47 -0700

    newly transferred game to Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('newproject1App')
+  .controller('MainCtrl', function ($scope) {
+	  $scope.boxes = [[{value: ""}, {value: ""}, {value: ""}], 
+	  [{value: ""}, {value: ""}, {value: ""}],
+	  [{value: ""}, {value: ""}, {value: ""}]];
+
+
+	  var myTurn = 1
+
+	  $scope.mark = function(cell) {
+	    // cell is passed in as an object
+	    // (so it's a reference to the original)
+
+	  if(myTurn % 2 == 0)
+	    cell.value = 'X';
+	  else
+	    cell.value = 'O';
+
+	  myTurn++;
+	  //   if(myTurn == 1)
+	  //    myTurn = 2;
+	  //   else
+	  //    myTurn = 1;
+
+	  };
+  });
