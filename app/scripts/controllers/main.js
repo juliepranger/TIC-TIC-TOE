74456a62282261b2f47183ba542871010df20ce6
Julie Pranger <juliepranger@Julies-MacBook-Pro.local>
2013-10-10 16:40:44 -0700

    queue staged, board intact, need to include then functions and restructure variables

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,41 +3,77 @@
 angular.module('newproject1App')
   .controller('MainCtrl', function ($scope, angularFire) {
 	  $scope.boxes = [];
+	  $scope.queue = {};
+	  $scope.gameId = - 1;
+	  $scope.playerNo = "";
 
-	  var database = new Firebase("https://tickytacky.firebaseio.com");
-	  angularFire(database, $scope, "boxes").then(function() {
-	  	$scope.boxes = [[{value: ""}, {value: ""}, {value: ""}], 
-		  [{value: ""}, {value: ""}, {value: ""}],
-		  [{value: ""}, {value: ""}, {value: ""}]];
+	  // window onload, new game button
+
+	  // $scope.window.onload = 
+
+//set up queue, games
+
+	  var boxesDatabase = new Firebase("https://tickytacky.firebaseio.com/boxes");
+	  angularFire(boxesDatabase, $scope, "boxes").then(function() {
+
+	  	var queue = new Firebase("https://tickytacky.firebaseio.com/queue");
+	  	angularFire(queue, $scope, "queue").then(function () {
+	  		if($scope.queue.gameId == undefined) {
+	  			console.log("I'm player 1!");
+	  			$scope.player = "p1";
+
+	  			var newGame = {
+	  				board: [[{value: ""}, {value: ""}, {value: ""}], 
+			  			[{value: ""}, {value: ""}, {value: ""}],
+			  			[{value: ""}, {value: ""}, {value: ""}]],
+	  				turn: 'p1',
+	  				over: false,
+	  				turnCount: 0,
+	  			};
+
+	  			$scope.gameId = $scope.boxes.push(newGame) - 1;
+	  			$scope.queue.gameId = $scope.gameId;
+	  			console.log("Player 1's game is: " + $scope.gameId);
+	  		} else {
+	  			console.log("I'm player two!");
+	  			$scope.player = "p2";
+
+	  			$scope.gameId = $scope.queue.gameId;
+	  			$scope.queue = {};
+	  			console.log("Player 2's game is: " + $scope.gameId);
+
+	  		}
+	  	});
 	  });
-	  var myTurn = 1
 
-	  $scope.won = false;
+	//   var myTurn = 1
 
-	  $scope.mark = function(cell) {
-	    // cell is passed in as an object
-	    // (so it's a reference to the original)
+	//   $scope.won = false;
 
-	    //if box is already taken, choose an empty box
-	if($scope.won == false) {
-	  if(cell.value == 'X' || cell.value == 'O')
-	  	alert("Please choose an empty cell!");
+	//   $scope.mark = function(cell) {
+	//     // cell is passed in as an object
+	//     // (so it's a reference to the original)
 
-	  //box isn't empty? great, we can determine Xs and Os now
+	//     //if box is already taken, choose an empty box
+	// if($scope.won == false) {
+	//   if(cell.value == 'X' || cell.value == 'O')
+	//   	alert("Please choose an empty cell!");
 
-	  else {
-		  if(myTurn % 2 == 0)
-		    cell.value = 'X';
-		  else
-		    cell.value = 'O';
+	//   //box isn't empty? great, we can determine Xs and Os now
 
-		  myTurn++
-		  };
-		  console.log("line 32");
-		  $scope.winCond(cell);
-		  console.log("line 34");
-	  };
-	}
+	//   else {
+	// 	  if(myTurn % 2 == 0)
+	// 	    cell.value = 'X';
+	// 	  else
+	// 	    cell.value = 'O';
+
+	// 	  myTurn++
+	// 	  };
+	// 	  console.log("line 32");
+	// 	  $scope.winCond(cell);
+	// 	  console.log("line 34");
+	//   };
+	// }
 
 //win conditions
 
@@ -52,22 +88,27 @@ angular.module('newproject1App')
 	   ($scope.boxes[0][1].value != "") && $scope.boxes[0][1].value == $scope.boxes[1][1].value && $scope.boxes[1][1].value == $scope.boxes[2][1].value ||
 	   ($scope.boxes[0][2].value != "") && $scope.boxes[0][2].value == $scope.boxes[1][2].value && $scope.boxes[1][2].value == $scope.boxes[2][2].value)
 		$scope.won = true;
-	
-	else 
-		console.log("this is working");
-		
-		if($scope.won == true) 
+
+// var cats = 0;
+
+// 	for(var row=0, row < $scope.boxes.length, ++row) {
+// 		for(var col=0, col < $scope.boxes.length, ++col) {
+// 			if($scope.boxes.value[row][col] != "")
+// 				cats++;
+// 		}
+// 	}
+// 	if(cats == 9 && $scope.won != true)
+// 		alert("Cat's game!");
+	// 	var total = $scope.boxes
+	// if(($scope.boxes.value != "1" && total = "9") && ($scope.won != true))
+
+
+
+	if($scope.won == true) 
 			alert("Congratulations! You win!");
 		}
 
 	})
-
-
-
-
-
-
-//room availability, player assignments
 
 
 
